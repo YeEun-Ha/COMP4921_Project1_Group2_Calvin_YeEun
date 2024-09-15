ac4e3acbb92a7b08f263bf185c8bc42de00d70b6
Calvin Vu <calvinvu20@gmail.com>
2024-09-14 19:13:54 -0700

    Add page from login and mantine UI

--- app/root.jsx
+++ app/root.jsx
@@ -1,30 +1,43 @@
 import {
-  Links,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-} from "@remix-run/react";
-import "./tailwind.css";
+    Links,
+    Meta,
+    Outlet,
+    Scripts,
+    ScrollRestoration,
+} from '@remix-run/react';
+import './tailwind.css';
+import { MantineProvider } from '@mantine/core';
+import '@mantine/core/styles.css';
+import Navbar from './components/common/navbar';
 
 export function Layout({ children }) {
-  return (
-    <html lang="en">
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <Meta />
-        <Links />
-      </head>
-      <body>
-        {children}
-        <ScrollRestoration />
-        <Scripts />
-      </body>
-    </html>
-  );
+    return (
+        <html lang='en'>
+            <head>
+                <meta charSet='utf-8' />
+                <meta
+                    name='viewport'
+                    content='width=device-width, initial-scale=1'
+                />
+                <Meta />
+                <Links />
+            </head>
+            <body>
+                {children}
+                <ScrollRestoration />
+                <Scripts />
+            </body>
+        </html>
+    );
 }
 
 export default function App() {
-  return <Outlet />;
+    return (
+        <MantineProvider withGlobalStyles withNormalizeCSS>
+            <Layout>
+                <Navbar />
+                <Outlet />
+            </Layout>
+        </MantineProvider>
+    );
 }
---
+++ app/routes/login.jsx
@@ -0,0 +1,9 @@
+import { LoginForm } from '../components/login';
+
+export const loader = async () => {
+    return null;
+};
+
+export default function LoginPage() {
+    return <LoginForm></LoginForm>;
+}
