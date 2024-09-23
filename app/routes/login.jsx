4db9047080345233b95a12ebc9d1adc14ef37c12
YeEun-Ha <hye8299@gmail.com>
2024-09-23 00:32:19 -0700

    check login error

--- app/routes/login.jsx
+++ app/routes/login.jsx
@@ -16,6 +16,7 @@ export const action = async ({ request, context }) => {
     const result = await postLogin(userPayload);
     if (result) {
         console.log('Login successfuly');
+        console.log('error, we dont have context. context:', context)
         context.session.userId = result.userID;
         context.session.username = result.username;
         context.session.authenticated = true;
